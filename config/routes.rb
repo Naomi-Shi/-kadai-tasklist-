805f5d88bfa82de82b1b73703c8817bae04535b9
naisui <naisuinaisui223@yahoo.co.jp>
2019-07-27 08:45:36 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
 #  get 'users/index'
 #  get 'users/show'
 #  get 'users/new'
 #  get 'users/create'
-  root to: 'toppages#index'
+#  root to: 'toppages#index'
 
-#root to: tasks#index
-#toppageは後でtasks#indexに戻す
+  root to: 'tasks#index'
+#toppageは後でtasks#indexに戻す。ログインか否かでページが変わるように設定
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
