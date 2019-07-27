5513366e086c6cb880bddd4246d85ec614701d04
naisui <naisuinaisui223@yahoo.co.jp>
2019-07-27 06:27:24 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-root to: 'toppages#index'
+#  get 'users/index'
+#  get 'users/show'
+#  get 'users/new'
+#  get 'users/create'
+  root to: 'toppages#index'
 
-#toppagesにルートを変えるため変更
-#root to: 'tasks#index'
+#root to: tasks#index
+#toppageは後でtasks#indexに戻す
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 	
 	resources :tasks
 end
